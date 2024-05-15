471e31c3d3954df8c334812f395ee1524e1247aa
iyiolacak <142077242+iyiolacak@users.noreply.github.com>
2024-05-15 16:44:01 +0300

    Cards update

--- src/app/(dashboard)/dashboard/components/Card.tsx
+++ src/app/(dashboard)/dashboard/components/Card.tsx
@@ -21,7 +21,22 @@ const Card = ({
   illustration,
 }: CardProps) => {
   return (
-    <div className="border bg-white shadow-sm rounded-xl w-[24rem] h-80">
+    <div className="border bg-white shadow-sm rounded-xl w-[24rem] h-80 p-6">
+        <div className="">
+
+        </div>
+        <div className="flex flex-col h-64 justify-end">
+            <h2 className="text-3xl font-bold">
+                December Report
+            </h2>
+            <p className="mt-2 text-slate-400 text-md w-80 mb-4">
+                Retrieve December report, analyze key data for informed strategic decisions. 
+            </p>
+            <div className="space-x-3">
+                <Button className="h-12 bg-[#4a38ba] rounded-xl" size={"lg"}>Analyze This</Button>
+                <Button className="h-12 border bg-[#d1cee5] rounded-xl text-black font-medium" size={"lg"}>Download</Button>
+            </div>
+        </div>
     </div>
   );
 };
--- src/app/(dashboard)/dashboard/page.tsx
+++ src/app/(dashboard)/dashboard/page.tsx
@@ -8,7 +8,7 @@ const Dashboard = () => {
     <div className="">
       <div>
         <WelcomeSection />
-        <div className="grid grid-cols-2 gap-6">
+        <div className="grid grid-cols-3 gap-6">
         <Card title="December Report" description="Retrieve December report, analyze key data for informed strategic decisions." mainContent={"test here"} footerContent={"footer content"} buttons={<button>Analyze</button>} gradient={""} illustration=""/>
         <Card title="December Report" description="Retrieve December report, analyze key data for informed strategic decisions." mainContent={"test here"} footerContent={"footer content"} buttons={<button>Analyze</button>} gradient={""} illustration=""/>
         <Card title="December Report" description="Retrieve December report, analyze key data for informed strategic decisions." mainContent={"test here"} footerContent={"footer content"} buttons={<button>Analyze</button>} gradient={""} illustration=""/>
