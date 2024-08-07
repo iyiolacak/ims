1a882c381a717356610f56037fca90890ecffd33
iyiolacak <142077242+iyiolacak@users.noreply.github.com>
2024-08-07 21:07:32 +0300

    - Inituitive "Bzzt" animation reaction effect on server error response. Improvements on ErrorDisplay reusable error displaying component and added more props for flexilibity. - Sign-in & sign-up pages are not Clerk hosted on system - Now, we're redirected to localhost:3000/sign-in instead of a Clerk hosted URL. - OTP is sent to API so it is functional. - Sign up custom flow works. Some required to dos exist. - Middleware modifications - Updated Next.js and @clerk/nextjs versions. - useAuthStatus custom hook is now providing us a "shake" data for inituitive UX reactions.

--- src/app/(authentication)/clerkErrorHandler.ts
+++ src/app/(authentication)/clerkErrorHandler.ts
@@ -1,13 +1,13 @@
 import { ClerkAPIError } from '@clerk/types';
 import { isClerkAPIResponseError } from '@clerk/nextjs/errors';
 
-export function handleClerkError(err: unknown): ClerkAPIError[] | null {
+export function getClerkError(err: unknown): ClerkAPIError[] | undefined {
     if(isClerkAPIResponseError(err)) {
-        console.error("Clerk API Error: ", JSON.stringify(err.errors, null, 2))
+        console.error(JSON.stringify(err.errors, null, 2))
         // Combine error messages into a single string
         return err.errors;
     } else {
         console.error("Unknown error: ", err);
-        return null;
+        return undefined;
     }
 }
